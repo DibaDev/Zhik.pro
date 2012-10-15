34607bcd8b98258a05488fa780e6e2ce24cff67c
mojiAm <amjadi.mojtaba@gmail.com>
2012-10-15 15:51:51 +0330

    layout 1(index)

--- moji/index.php
+++ moji/index.php
@@ -8,49 +8,53 @@
 	<link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
-	<div class="container">
-		<header class="header">
-			<div class="hline"></div>
-			<div class="head">
-				<div class="logo"></div>
-				<div class="search"></div>
-			</div>
-			<nav class="menu"></nav>
-		</header>
-		<section class="banner">
-			<div class="slideshow"></div>
-			<div class="text"></div>
-		</section>
-		<section class="middle1">
-			<div class="first"></div>
-			<div class="second"></div>
-			<div class="third"></div>
-		</section>
-		<section class="middle2">
-			<div class="first"></div>
-			<div class="second"></div>
-			<div class="third"></div>
-		</section>
-		<footer class="footer">
-			<div class="first">
-				<div class="copyright"></div>
-				<div class="menu"></div>
-			</div>
-			<div class="second">
-				<div class="header"></div>
+	<div class="contain">
+		<div class="hline"></div>
+		<div class="container">
+			<header class="header">
+				<div class="head">
+					<div class="logo"></div>
+					<div class="search"></div>
+				</div>
+				<nav class="menu"></nav>
+			</header>
+			<section class="banner">
+				<div class="slideshow"></div>
 				<div class="text"></div>
-			</div>
-			<div class="third">
-				<div class="header"></div>
-				<div class="schub1">
-					<div class="fa"></div>
-					<div class="tw"></div>
+			</section>
+			<section class="middle1">
+				<div class="first"></div>
+				<div class="second"></div>
+				<div class="third"></div>
+			</section>
+			<section class="middle2">
+				<div class="first"></div>
+				<div class="second"></div>
+				<div class="third"></div>
+			</section>
+		</div>
+		<footer class="contain">
+			<section class="footer container">
+				<div class="first">
+					<div class="copyright"></div>
+					<div class="menu"></div>
 				</div>
-				<div class="schub2">
-					<div class="gp"></div>
-					<div class="rs"></div>
+				<div class="second">
+					<div class="header"></div>
+					<div class="text"></div>
 				</div>
-			</div>
+				<div class="third">
+					<div class="header"></div>
+					<div class="schub1">
+						<div class="fa"></div>
+						<div class="tw"></div>
+					</div>
+					<div class="schub2">
+						<div class="gp"></div>
+						<div class="rs"></div>
+					</div>
+				</div>
+			</section>
 		</footer>
 	</div>
 </body>
